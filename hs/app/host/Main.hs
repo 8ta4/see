781b4be7582d0999b074af1d40d7b7e905d91461
a <a@as-Virtual-Machine.local>
2025-09-27 00:28:55 -0700

    Prevent bind errors by deleting stale UNIX socket if present

--- hs/app/host/Main.hs
+++ hs/app/host/Main.hs
@@ -1,13 +1,25 @@
 module Main (main) where
 
+import Control.Exception (catch, throwIO)
 import Network.Socket (Family (AF_UNIX), SockAddr (SockAddrUnix), SocketType (Stream), defaultProtocol, socket)
 import Network.Socket.Address (bind)
 import Relude
-import System.Directory (getTemporaryDirectory)
+import System.Directory (getTemporaryDirectory, removeFile)
 import System.FilePath ((</>))
+import System.IO.Error (isDoesNotExistError)
+
+-- https://stackoverflow.com/a/8502391
+removeIfExists :: FilePath -> IO ()
+removeIfExists fileName = removeFile fileName `catch` handleExists
+  where
+    handleExists e
+      | isDoesNotExistError e = return ()
+      | otherwise = throwIO e
 
 main :: IO ()
 main = do
   temporaryDirectory <- getTemporaryDirectory
   unixSocket <- socket AF_UNIX Stream defaultProtocol
-  bind unixSocket $ SockAddrUnix $ temporaryDirectory </> "see.sock"
+  let socketPath = temporaryDirectory </> "see.sock"
+  removeIfExists socketPath
+  bind unixSocket $ SockAddrUnix socketPath
