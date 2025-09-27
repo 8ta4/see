e9852b6d913c0df56814e8e77531956dca93d5a5
a <a@as-Virtual-Machine.local>
2025-09-27 02:20:46 -0700

    Write Native Messaging host manifest to user's home directory

--- hs/app/see/Main.hs
+++ hs/app/see/Main.hs
@@ -1,8 +1,10 @@
 module Main (main) where
 
+import Data.Aeson (KeyValue ((.=)), encode, object)
 import Options.Applicative (execParser, strArgument)
 import Options.Applicative.Builder (info)
 import Relude
+import System.Directory (createDirectoryIfMissing, getHomeDirectory)
 import System.Environment (getExecutablePath)
 import System.FilePath (takeFileName, (</>))
 
@@ -11,6 +13,10 @@ main = do
   maybeDevenvRoot <- lookupEnv "DEVENV_ROOT"
   seePath <- getExecutablePath
   let hostPath = maybe seePath (</> "hs/bin") (guarded ((== "see") . takeFileName) =<< maybeDevenvRoot) </> "host"
+  homeDirectory <- getHomeDirectory
+  let nativeMessagingHostsPath = homeDirectory </> "Library/Application Support/Mozilla/NativeMessagingHosts"
+  createDirectoryIfMissing True nativeMessagingHostsPath
+  writeFileLBS (nativeMessagingHostsPath </> "see.json") $ encode $ object ["path" .= hostPath]
   url <- execParser $ info (strArgument mempty) mempty
   putTextLn "Processing URL:"
   putTextLn url
