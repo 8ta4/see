6e64917b1c67526a08f47bda2f331e8ef2458985
a <a@as-Virtual-Machine.local>
2025-09-27 00:09:09 -0700

    Replace directory creation with Unix domain socket binding in Main

--- hs/app/host/Main.hs
+++ hs/app/host/Main.hs
@@ -1,9 +1,13 @@
 module Main (main) where
 
+import Network.Socket (Family (AF_UNIX), SockAddr (SockAddrUnix), SocketType (Stream), defaultProtocol, socket)
+import Network.Socket.Address (bind)
 import Relude
-import System.Directory (createDirectoryIfMissing, getTemporaryDirectory)
+import System.Directory (getTemporaryDirectory)
+import System.FilePath ((</>))
 
 main :: IO ()
 main = do
   temporaryDirectory <- getTemporaryDirectory
-  createDirectoryIfMissing False $ temporaryDirectory <> "see"
+  unixSocket <- socket AF_UNIX Stream defaultProtocol
+  bind unixSocket $ SockAddrUnix $ temporaryDirectory </> "see.sock"
