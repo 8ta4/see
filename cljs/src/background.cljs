2d6c8d1dac27eecef19a3105e84981d1b5aaca19
a <a@as-Virtual-Machine.local>
2025-09-30 00:48:30 -0700

    Reformat host message logging block

--- cljs/src/background.cljs
+++ cljs/src/background.cljs
@@ -3,10 +3,9 @@
 (defonce port
   (js/browser.runtime.connectNative "host"))
 
-(port.onMessage.addListener
- (fn [message]
-   (println "Message from host:")
-   (println message)))
+(port.onMessage.addListener (fn [message]
+                              (println "Message from host:")
+                              (println message)))
 
 (defn init []
   (println "Hello, World!"))
