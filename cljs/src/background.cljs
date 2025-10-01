392c41a615c057f46b08e76dcb5de67b397533f3
a <a@as-Virtual-Machine.local>
2025-09-30 19:45:45 -0700

    Add state atom to track tab status

--- cljs/src/background.cljs
+++ cljs/src/background.cljs
@@ -4,12 +4,19 @@
 (defonce port
   (js/chrome.runtime.connectNative "host"))
 
+(def state
+  (atom nil))
+
+(defn handle-tab-update
+  [_ _ tab]
+  (reset! state (:status (js->clj tab :keywordize-keys true))))
+
 (defn handle-host
   [message]
   (js/console.log "Message from host:")
   (js/console.log message)
   (js-await [tab (js/chrome.tabs.create (clj->js {}))]
-            (:id (js->clj tab :keywordize-keys true))))
+            (js/chrome.tabs.onUpdated.addListener handle-tab-update (clj->js {:tabId (:id (js->clj tab :keywordize-keys true))}))))
 
 (port.onMessage.addListener handle-host)
 
