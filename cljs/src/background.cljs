0ffe65c04ba93a830b03a3ac2d7e81d22cb85a69
a <a@as-Virtual-Machine.local>
2025-09-30 19:21:45 -0700

    Add clj-kondo config to lint js-await as let

--- cljs/src/background.cljs
+++ cljs/src/background.cljs
@@ -1,4 +1,5 @@
-(ns background)
+(ns background
+  (:require [shadow.cljs.modern :refer [js-await]]))
 
 (defonce port
   (js/chrome.runtime.connectNative "host"))
@@ -7,7 +8,8 @@
   [message]
   (js/console.log "Message from host:")
   (js/console.log message)
-  (js/chrome.tabs.create (clj->js {})))
+  (js-await [tab (js/chrome.tabs.create (clj->js {}))]
+            (:id (js->clj tab :keywordize-keys true))))
 
 (port.onMessage.addListener handle-host)
 
